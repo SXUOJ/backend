26291e68a33933c15b044fb58bdedd0ec0f5c59b
jiaomaster <1046726249@qq.com>
2023-01-03 20:53:28 +0800

    SampleUpdate

--- logic/sample.go
+++ logic/sample.go
@@ -1,11 +1,19 @@
 package logic
 
-import "net/http"
+import (
+	"fmt"
+	"github.com/spf13/viper"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+	"web_app/pkg/uuid"
+)
 
 func SampleHandler(w http.ResponseWriter, r *http.Request) {
 	m := r.Method
 	if m == http.MethodPost {
-		Put(w, r)
+		SPut(w, r)
 		return
 	}
 	if m == http.MethodGet {
@@ -18,4 +26,61 @@ func SampleHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	w.WriteHeader(http.StatusMethodNotAllowed)
 
+}
+
+func SPut(w http.ResponseWriter, r *http.Request) {
+	contentType := r.Header.Get("content-type")
+	contentLen := r.ContentLength
+
+	fmt.Printf("upload content-type:%s,content-length:%d", contentType, contentLen)
+	if !strings.Contains(contentType, "multipart/form-data") {
+		w.Write([]byte("content-type must be multipart/form-data"))
+		return
+	}
+	if contentLen >= 300*1024*1024 { // 300 MB
+		w.Write([]byte("file to large,limit 300MB"))
+		return
+	}
+	err := r.ParseMultipartForm(50 * 1024 * 1024)
+	if err != nil {
+		//http.Error(w, err.Error(), http.StatusInternalServerError)
+		w.Write([]byte("ParseMultipartForm error:" + err.Error()))
+		return
+	}
+
+	if len(r.MultipartForm.File) == 0 {
+		w.Write([]byte("not have any file"))
+		return
+	}
+	id, _ := uuid.Getuuid()
+	exten, _ := getContentType(strings.Split(r.URL.EscapedPath(), "/")[5])
+	fmt.Println(id, exten)
+	//log.Println(r.URL.EscapedPath())
+	//C:\Users\Administrator\go\src\awesomeProject\test_file
+	f, _ := os.OpenFile("./file"+"/img/"+id+"."+exten, os.O_RDWR|os.O_CREATE, 0755)
+	defer f.Close()
+	for name, files := range r.MultipartForm.File {
+		fmt.Printf("req.MultipartForm.File,name=%s", name)
+
+		if len(files) != 1 {
+			w.Write([]byte("too many files"))
+			return
+		}
+		if name == "" {
+			w.Write([]byte("is not FileData"))
+			return
+		}
+		for _, ff := range files {
+			handle, err := ff.Open()
+			if err != nil {
+				w.Write([]byte(fmt.Sprintf("unknown error,fileName=%s,fileSize=%d,err:%s", ff.Filename, ff.Size, err.Error())))
+				return
+			}
+
+			io.Copy(f, handle)
+			fmt.Printf("successful uploaded,fileName=%s,fileSize=%.2f MB,savePath=%s \n", id+"."+exten, float64(contentLen)/1024/1024, f.Name())
+
+			w.Write([]byte("http://" + viper.GetString("app.server") + ":" + viper.GetString("app.port") + "/api/admin/upload/image/" + id + "." + exten))
+		}
+	}
 }
--- models/question.go
+++ models/question.go
@@ -9,35 +9,27 @@ type QuestionSql struct {
 
 // Question
 type Question struct {
-	Context     Context     `json:"context"`
+	Context     Empty       `json:"context"`
 	Information Information `json:"information"`
 	Limit       Limit       `json:"limit"`
 	Title       string      `json:"title"` // 标题
 }
 
-type QuestionList struct {
-	Title  string `json:"title"` // 标题
-	ID     string `json:"id"`
-	Status string `json:"status"`
-}
-
-type Context struct {
-	Description       string `json:"description"` // 描述
-	ImgPath           string `json:"img_path"`
-	Input             string `json:"input"`               // 输入描述
-	InputSample       string `json:"input_sample"`        // 输入样例
-	InputSamplesPath  string `json:"input_samples_path"`  // 输入样例路径
-	Output            string `json:"output"`              // 输出描述
-	OutputSample      string `json:"output_sample"`       // 输出样例
-	OutputSamplesPath string `json:"output_samples_path"` // 输出样例路径
-	Source            string `json:"source"`              // 来源
+type Empty struct {
+	Description  string   `json:"description"` // 描述
+	ImgPath      string   `json:"img_path"`
+	Input        string   `json:"input"`         // 输入描述
+	InputSample  []string `json:"input_sample"`  // 输入样例
+	Output       string   `json:"output"`        // 输出描述
+	OutputSample []string `json:"output_sample"` // 输出样例
+	Source       string   `json:"source"`        // 来源
 }
 
 type Information struct {
-	Creator    string `json:"creator"`                               // 创建者
-	Level      string `json:"level"`                                 // 等级
-	QuestionID string `json:"question_id" gorm:"question_id;unique"` // 问题ID
-	Tags       string `json:"tags"`                                  // 标签
+	Creator    string `json:"creator"`     // 创建者
+	Level      string `json:"level"`       // 等级
+	QuestionID string `json:"question_id"` // 问题ID
+	Tags       string `json:"tags"`        // 标签
 }
 
 type Limit struct {
