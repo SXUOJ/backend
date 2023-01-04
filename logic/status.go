08a3f93a9ecc8c36002e4b3a7c516aa4e624966d
jiaomaster <1046726249@qq.com>
2023-01-04 17:16:23 +0800

    main

--- logic/status.go
+++ logic/status.go
@@ -1,11 +1,16 @@
 package logic
 
-import "web_app/models"
+import (
+	"web_app/dao/mysql"
+	"web_app/models"
+)
 
 func GetStatusList(qid string, uid string, amount string, page string) ([]*models.Result, error) {
-	return nil, nil
+	re, err := mysql.GetStatusListByQid(qid, uid, amount, page)
+	return re, err
 }
 
 func GetStatusDetail(sid string) (models.Result, error) {
-	return *new(models.Result), nil
+	re, err := mysql.GetStatusByAdmitId(sid)
+	return re, err
 }
