ef67136bc83b37aba680cf20c8a5152af1b413bb
isther <28251536@qq.com>
2023-01-12 21:23:37 +0800

    To get addr list

--- dao/judger.go
+++ dao/judger.go
@@ -6,6 +6,19 @@ func InsertJudger(addr models.JudgerAddr) error {
 	return db.Create(&models.JudgerAddrSql{JudgerAddr: addr}).Error
 }
 
-func GetJudger() (addrs []*models.JudgerAddr, err error) {
-	return nil, nil
+func GetJudger() ([]*models.JudgerAddr, error) {
+	var (
+		addrSqls []*models.JudgerAddrSql
+		addrs    []*models.JudgerAddr
+	)
+
+	if err := db.Find(&addrs).Error; err != nil {
+		return nil, err
+	}
+
+	for i := range addrSqls {
+		addrs = append(addrs, &addrSqls[i].JudgerAddr)
+	}
+
+	return addrs, nil
 }
