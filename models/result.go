9a3122f016a18d2bec6bf55defb1d5901b2aebc2
isther <28251536@qq.com>
2023-01-12 19:59:04 +0800

    Modify models

--- models/result.go
+++ models/result.go
@@ -1,6 +1,9 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"github.com/lib/pq"
+	"gorm.io/gorm"
+)
 
 type ResultSql struct {
 	gorm.Model
@@ -25,19 +28,19 @@ const (
 
 // result
 type Result struct {
-	SubmitID   string `json:"submit_id"`
-	CodeId     string `json:"code_id"`
-	UserID     string `json:"user_id"`
-	QuestionID string `json:"question_id"`
-	Time       string `json:"time"`
-	IfAC       bool   `json:"if_ac"`
-	CodeType   uint64 `json:"code_type"` // 代码类型
-	Source     string `json:"source"`    // 源码
-	Public     int64  `json:"public"`
-	Results    []ResultOne
+	SubmitID   string        `json:"submit_id"`
+	CodeId     string        `json:"code_id"`
+	UserID     string        `json:"user_id"`
+	QuestionID string        `json:"question_id"`
+	Time       string        `json:"time"`
+	IfAC       bool          `json:"if_ac"`
+	CodeType   uint64        `json:"code_type"` // 代码类型
+	Source     string        `json:"source"`    // 源码
+	Public     int64         `json:"public"`
+	Results    pq.ByteaArray `json:"results"`
 }
 
-type ResultOne struct {
+type ResultOfOneSample struct {
 	Status   uint64 `json:"status"`
 	Memory   string `json:"memory"`
 	RealTime string `json:"real_time"`
@@ -46,14 +49,25 @@ type ResultOne struct {
 }
 
 // 非入库数据模型
-type Code struct {
-	CodeID      string `json:"code_id"`   // 本次提交代码ID
+type Submit struct {
+	QuestionID string `json:"question_id"` // 题目ID
+	//TODO: CodeID
+	// CodeID      string `json:"code_id"`   // 本次提交代码ID
+	UserID      string `json:"user_id"`   // 用户ID
 	CodeType    uint64 `json:"code_type"` // 代码类型
 	Public      int64  `json:"public"`
-	QuestionID  string `json:"question_id"` // 题目ID
-	Source      string `json:"source"`      // 源码
-	Time        string `json:"time"`        // 提交时间
-	UserID      string `json:"user_id"`     // 用户ID
+	Source      string `json:"source"` // 源码
+	Time        string `json:"time"`   // 提交时间
 	TimeLimit   uint64 `json:"time_limit"`
 	MemoryLimit uint64 `json:"memory_limit"`
+}
+
+type SubmitResult struct {
+	SubmitID   string              `json:"submit_id"`
+	CodeId     string              `json:"code_id"`
+	UserID     string              `json:"user_id"`
+	QuestionID string              `json:"question_id"`
+	Time       string              `json:"time"`
+	IfAC       bool                `json:"if_ac"`
+	Results    []ResultOfOneSample `json:"results"`
 }
